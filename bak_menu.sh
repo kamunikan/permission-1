ff9e8e943eff647310757cfe3f6d77df3c9fbacd
anzclan <nor4ini96@gmail.com>
2022-09-05 20:50:03 +0800

    register

---
+++ bak_menu.sh
@@ -0,0 +1,13 @@
+mkdir /root/menu_all
+cp /usr/bin/menu /root/menu_all/menu.sh
+cp /usr/bin/menu-ss /root/menu_all/menu-ss.sh
+cp /usr/bin/menu-vmess /root/menu_all/menu-vmess.sh
+cp /usr/bin/menu-vless /root/menu_all/menu-vless.sh
+cp /usr/bin/menu-trojan  /root/menu_all/menu-trojan .sh
+cp /usr/bin/menu-bot /root/menu_all/menu-bot.sh
+cp /usr/bin/menu-ssh /root/menu_all/menu-ssh.sh
+cp /usr/bin/menu-set /root/menu_all/menu-set.sh
+cp /usr/bin/menu-theme /root/menu_all/menu-theme.sh
+cp /usr/bin/menu-ip /root/menu_all/menu-ip.sh
+cp /usr/bin/menu-backup  /root/menu_all/menu-backup.sh
+cp /usr/bin/autoboot /root/menu_all/autoboot.sh
